9ea6b6366b5b6f0a32d0e9abe40d27206dc7739a
shravan097 <shravan097@users.noreply.github.com>
2021-10-24 23:52:02 -0400

    push changes

---
+++ qna/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { QuestionsModule } from './questions/questions.module';
+import { AnswersModule } from './answers/answers.module';
+
+@Module({
+  imports: [QuestionsModule, AnswersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
